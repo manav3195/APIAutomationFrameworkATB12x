4555f26818e80e5a4ed0e48a631425023cca1f28
manav3195 <manavchaudhary3195@gmail.com>
2026-02-02 22:40:43 +0530

    Commit

---
+++ src/test/java/com/thetestingacademy/tests/sample/RestAssured_HelloWorld.java
@@ -0,0 +1,9 @@
+package com.thetestingacademy.tests.sample;
+
+public class RestAssured_HelloWorld {
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+    }
+}
